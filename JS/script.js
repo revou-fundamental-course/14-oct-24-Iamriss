a22d9287dd1107f1170db755a644c2554449b321
unknown <moriswirantara2003@gmail.com>
2024-10-20 15:38:55 +0700

    first commit

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+// ini js
+
+
+function validasi() {
+    var nama = document.getElementById("nama").value;
+    var tanggallahir = document.getElementById("tanggallahir").value;
+    var jenisKelamin = document.getElementById("jenisKelamin").value;
+    var pesan = document.getElementById("pesan").value;
+
+    if (nama !="" && tanggallahir !="" && jenisKelamin !="" && pesan !="") {
+        return true;
+    } else {
+        alert("Nama harus terisi");
+    }
+}
