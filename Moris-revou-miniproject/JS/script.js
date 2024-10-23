6d9417fe48e4868b79fa932d51f2fa2cc51c6d45
Iamriss <168885399+Iamriss@users.noreply.github.com>
2024-10-24 00:57:58 +0700

    Add files via upload

---
+++ Moris-revou-miniproject/JS/script.js
@@ -0,0 +1,18 @@
+// ini js
+
+
+// function validasi() {
+//     var nama = document.getElementById("nama").value;
+//     var tanggallahir = document.getElementById("tanggallahir").value;
+//     var jenisKelamin = document.getElementById("jenisKelamin").value;
+//     var pesan = document.getElementById("pesan").value;
+
+//     if (nama !="" && tanggallahir !="" && jenisKelamin !="" && pesan !="") {
+//         return true;
+//     } else {
+//         alert("Nama harus terisi");
+//     }
+// }
+
+var namakamu = window.prompt("masukan nama kamu");
+
